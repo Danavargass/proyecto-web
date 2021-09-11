0de3f1f8f6b87a1c4027aeec18dd9ad557898e58
Danavargass <87346665+Danavargass@users.noreply.github.com>
2021-09-10 19:17:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cars', { delay: 300});
+ScrollReveal().reveal('.Juegos.html', { delay: 300});
+ScrollReveal().reveal('.Noticias.html', { delay: 300});
+ScrollReveal().reveal('.Guias.html', { delay: 300});
+ScrollReveal().reveal('.Lanzamientos.html', { delay: 300});
